d8babefdc2460d9bc5116d904d97ecfbb7ded450
Daniel J. Paulino <37070026+daniel-jp@users.noreply.github.com>
2021-11-06 02:16:02 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,33 @@
 const linksSocialMedia = {
-    github: 'Daniel-jp',
-    youtube: 'UCWpSVjfuqZhQZ5gfyFg56jw',
-    facebook: 'danieljoaquimpaulino.danny',
-    instagram: 'danny_arm_amy',
-    twitter: 'maykbrito'
+  github: 'Daniel-jp',
+  youtube: 'UCWpSVjfuqZhQZ5gfyFg56jw',
+  facebook: 'danieljoaquimpaulino.danny',
+  instagram: 'danny_arm_amy',
+  twitter: 'Daniel28725148',
+  linkedin: 'daniel-joaquim-paulino-737808198'
 }
 
 function changeSocialMidiaLinks() {
-    for (let li of socialLinks.children) {
-        const social = li.getAttribute('class')
-        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
-            //alert(li.children[0].href)
-    }
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    //alert(li.children[0].href)
+  }
 }
 changeSocialMidiaLinks()
 
+//using APi
+
 function getGithubProfileInfos() {
-    const url = `https://api.github.com/users/${linksSocialMedia.github}`
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            userName.textContent = data.name
-            userBio.textContent = data.bio
-            userLink.href = data.html_url
-            userImage.src = data.avatar_url
-            userLogin.textContent = data.login
-        })
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
 }
 getGithubProfileInfos()
